c1cd05a80362916ecab85d88ea947de0267dddc8
Dell <muhsindev4@gmail.com>
2022-08-09 15:32:36 +0530

    Added Procfile to setup the dyno

--- bin/server.dart
+++ bin/server.dart
@@ -10,7 +10,7 @@ final _router = Router()
   ..get('/echo/<message>', _echoHandler);
 
 Response _rootHandler(Request req) {
-  return Response.ok('Hello, Wodjrld!\n');
+  return Response.ok('Hello, Wodadefsegjrld!\n');
 }
 
 Response _echoHandler(Request request) {
