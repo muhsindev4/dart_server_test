d08df8f5f100c5584b5bba44efb91920c265a61c
Dell <muhsindev4@gmail.com>
2022-08-09 15:45:30 +0530

    secd

--- bin/server.dart
+++ bin/server.dart
@@ -19,14 +19,11 @@ Response _echoHandler(Request request) {
 }
 
 void main(List<String> args) async {
-  // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
-
-  // Configure a pipeline that logs requests.
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  var env = Platform.environment;
 
-  // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
+  var port = env.entries.firstWhere((element) => element.key == 'PORT',
+      orElse: () => MapEntry('PORT', '8080'));
+  final server = await serve(logRequests().addHandler(handler), '0.0.0.0', int.parse(port.value));
   print('Server listening on port ${server.port}');
 }
